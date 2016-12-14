7e4e0493e4195d22d6b1e1b07006ccfd2c19316b
hniah <hoangngochai@gmail.com>
2016-12-14 10:59:57 +0700

    run migrate create category and blog"

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212151928) do
+ActiveRecord::Schema.define(version: 20161214035253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_tables", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "photo"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "category_tables", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
