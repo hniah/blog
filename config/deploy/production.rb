3e5a755717df897f7ed156ffbc51c2010f2e8534
Anthony <hoangngochai@gmail.com>
2019-01-08 13:02:32 +0700

    Update deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,8 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
+server '18.224.229.98', user: 'deploy', roles: [:web, :app], primary: true
+set :branch, 'master'
 
 # role-based syntax
 # ==================
